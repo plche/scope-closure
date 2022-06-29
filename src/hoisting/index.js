59c204feb74e114125be2c920434ebc60b69d494
pchec <che.percy.l@gmail.com>
2022-06-29 15:39:57 -0500

    Understanding Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+/*a = 2;
+var a;
+console.log(a);*/
+
+/*console.log(a); // undefined
+var a = 2;*/
+
+nameOfDog('Elmo'); // Elmo gets printed, because of hoisting the function declaration
+
+function nameOfDog(name) {
+    console.log(name);
+}
